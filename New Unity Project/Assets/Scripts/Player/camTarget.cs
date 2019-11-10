05d6d90e09622b660e9175ab1d9d605355981d70
Ricky-James <rickyjamesirl@gmail.com>
2019-11-10 02:28:34 +0000

    Initial Commit.
    Project already WIP. Just setting up Github alongside Unity Collab for branching purposes

---
+++ New Unity Project/Assets/Scripts/Player/camTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camTarget : MonoBehaviour
+{
+    public Transform playerTarget;
+
+    private float rotSpeed = 1.5f;
+    private float distance = 10f;
+
+    float xPos, zPos;
+
+    private float timer = 0;
+
+    private Vector3 targetOffset = new Vector3(-10, 1.5f, -10);
+
+
+    void Update()
+    {
+        float direction = Input.GetAxisRaw("Horizontal");
+        timer -= Time.deltaTime * rotSpeed * direction;
+
+
+        xPos = Mathf.Cos(timer) * distance;
+        zPos = Mathf.Sin(timer) * distance;
+        targetOffset = new Vector3(xPos, 1.5f, zPos);
+
+
+        transform.localPosition = playerTarget.position + targetOffset;
+
+
+
+        Debug.Log("Trans pos: " + transform.position);
+        Debug.Log("Offset: " + targetOffset);
+    }
+
+
+}
---
+++ New Unity Project/Assets/Scripts/Scrapped Scripts/playerDirections.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDirections : MonoBehaviour
+{
+    public Transform player;
+
+    private void Update()
+    {
+  //      this.transform.position = player.transform.position;
+        this.transform.SetPositionAndRotation(player.transform.position,
+            new Quaternion(player.transform.rotation.x, player.transform.rotation.y, 0, 0));
+    }
+
+}
